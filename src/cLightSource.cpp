c474553144fc2ad37a22d8806464368489717dd2
geewy <44976737+geewy@users.noreply.github.com>
2018-11-12 17:41:36 +0100

    Librairie of a light source

---
+++ src/cLightSource.cpp
@@ -0,0 +1,38 @@
+/**
+ * \file cLightSource.cpp
+ * \brief Librairie of a light source
+ *
+ * \author geewy
+ */
+#include "stdafx.h"
+#include "cLightSource.h"
+
+/**
+ * \fn cLightSource::cLightSource()
+ * \brief Contructor of a light source
+ */
+cLightSource::cLightSource(){}
+
+
+/**
+ * \fn cLightSource::~cLightSource()
+ * \brief Destructor of a light source
+ */
+cLightSource::~cLightSource(){}
+
+cVector cLightSource::getPos(){
+	return m_position;
+}
+
+void cLightSource::setPos(cVector pos){
+	m_position = pos;
+}
+
+
+void cLightSource::setColorLight(float red, float green, float blue){
+	m_color.setColor(red, green, blue);
+}
+
+cVector cLightSource::getColor(){
+	return m_color;
+}
