2817e843ebc8035dd64906efb62f2d2c7f24e8f7
geewy <44976737+geewy@users.noreply.github.com>
2018-11-13 10:47:59 +0100

    Librairy of a ponctual light source

---
+++ src/cPonctual.cpp
@@ -0,0 +1,20 @@
+/**
+ * \file cPonctual.cpp
+ * \brief Librairy of a ponctual light source
+ *
+ * \author geewy
+ */
+#include "stdafx.h"
+#include "cPonctual.h"
+
+/**
+ * \fn cPonctual::cPonctual()
+ * \brief Contructor of a ponctual light source
+ */
+cPonctual::cPonctual(){}
+
+/**
+ * \fn cPonctual::~cPonctual()
+ * \brief Destructor of a ponctual light source
+ */
+cPonctual::~cPonctual(){}
