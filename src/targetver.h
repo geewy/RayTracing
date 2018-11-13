e3bf805f9afd96a58ac4d1ac5cb15af492de94e9
geewy <44976737+geewy@users.noreply.github.com>
2018-11-13 11:00:36 +0100

    defines the last available Windows platform

---
+++ src/targetver.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// If you include SDKDDKVer.h, this defines the last available Windows platform.
+
+// If you want to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you want to support before including SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
