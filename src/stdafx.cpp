fd68b4c36a8e65704bcdd24f6ea41f7fe909fd13
geewy <44976737+geewy@users.noreply.github.com>
2018-11-13 11:00:04 +0100

    source file just including standard include files

---
+++ src/stdafx.cpp
@@ -0,0 +1,5 @@
+// stdafx.cpp: source file just including standard include files
+// Raytracing.pch represents the precompiled header
+// stdafx.obj contains precompiled type information
+
+#include "stdafx.h"
